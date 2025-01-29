9a6ce03df116ab4f5ed341620b2c10eab15c9c45
Pahima R Uchil <uchilpahi@gmail.com>
2025-01-29 11:19:40 +0530

    Basic Javascript Fundamentals

---
+++ script.js
@@ -0,0 +1,59 @@
+console.log("Pahima");
+console.log(19);
+
+let firstName = "Pahima";
+
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+console.log(myFirstJob);
+
+//datatypes
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'YES!'; //Need not use 'let' while assigning again 
+console.log(typeof javascriptIsFun);
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+//const and var
+const birthYear = 1991; //better to use 'const' for constant variables rather that 'let'
+
+//var almost works same as let bbut not recommended to use
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Uchil';
+console.log(lastName);
+
+
+//Operators
+const presentYear =2027;
+const myAge = presentYear -birthYear;
+console.log(myAge);
+
+//Math operators
+console.log(myAge*2,myAge/4,2**4);
+
+console.log(firstName+' '+lastName);
+
+
+// Assignment operators
+let x;
+x += 10; 
+x *= 4;
+
+
+
+
