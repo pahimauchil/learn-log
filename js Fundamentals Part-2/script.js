b371d70c05d8789d1c6fcc42f156f6bd3c2d91de
Pahima R Uchil <uchilpahi@gmail.com>
2025-01-30 22:38:49 +0530

    arrays

--- js Fundamentals Part-2/script.js
+++ js Fundamentals Part-2/script.js
@@ -102,8 +102,8 @@ console.log(yearsUntilRetirement(1950, 'Roy'));
 
 const calcAverage = (a,b,c)=>(a+b+c)/3;
 
- const avgDolphins = calcAverage(44,23,71);
- const avgKoalas = calcAverage(85,5,41);   
+ let avgDolphins = calcAverage(44,23,71);
+ let avgKoalas = calcAverage(85,5,41);   
 
  const checkwinner = function(avgDolphins,avgKoalas){
     if(avgDolphins>=(2*avgKoalas)){
@@ -117,3 +117,65 @@ const calcAverage = (a,b,c)=>(a+b+c)/3;
  checkwinner(avgDolphins,avgKoalas);
  checkwinner(576,111);
 
+ //Arrays
+
+const friends = ['Steve','Michael','Peter'];
+console.log(friends);
+
+ //another way 
+const y =new Array(190,192,193);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+
+  //to get last element of array
+
+ console.log(friends[friends.length - 1]);
+
+ //changing a value in array
+ friends[3] = 'Jay';
+ console.log(friends);
+
+ const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const ages1 = calcAge(years[0]);
+const ages2 = calcAge(years[1]);
+const ages3 = calcAge(years[years.length - 1]);
+console.log(ages1, ages2, ages3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+
+
+// Basic Array Operations 
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); 
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
